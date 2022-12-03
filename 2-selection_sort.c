d802a2d323aff702c3e404f99ebf8b7b42f619c0
A-simpa <mohammedabdulmujeeb16@gmail.com>
2022-12-03 03:26:14 +0000

    implemented selection sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,32 @@
+#include "sort.h"
+
+/**
+ * selection_sort - sort an array using buble sort
+ *
+ * @array: the array to sort
+ * @size: size of the array
+ *
+ * Return: void
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	int min, pos;
+	size_t n, i;
+
+	n = 0;
+	while (n < size - 1)
+	{
+		min = array[n];
+		for (i = n + 1; i < size; i++)
+		{
+			if (array[i] < min)
+				min = array[i], pos = i;
+		}
+		array[pos] = array[n];
+		array[n] = min;
+		print_array(array, size);
+		n++;
+	}
+}
+
